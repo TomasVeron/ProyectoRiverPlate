90e74bf661306384cdaf3b1a119a33e56f7a4370
facundojure <72893752+facundojure@users.noreply.github.com>
2021-12-08 19:57:18 -0300

    implementado listar socios

--- src/main/java/ar/edu/unnoba/proyecto_river_plate_junin/controller/SocioController.java
+++ src/main/java/ar/edu/unnoba/proyecto_river_plate_junin/controller/SocioController.java
@@ -1,10 +1,25 @@
 package ar.edu.unnoba.proyecto_river_plate_junin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ar.edu.unnoba.proyecto_river_plate_junin.service.SocioService;
 
 @Controller
 public class SocioController {
 
+@Autowired
+private SocioService socioService;
+@GetMapping("/socios")
+public String sociosView(Model model){
+    model.addAttribute("socios",socioService.getAllSocios());
+    return "/socios";
 
 
-}
+
+}}
+
+
+
--- src/main/java/ar/edu/unnoba/proyecto_river_plate_junin/service/SocioServiceImp.java
+++ src/main/java/ar/edu/unnoba/proyecto_river_plate_junin/service/SocioServiceImp.java
@@ -1,8 +1,25 @@
 package ar.edu.unnoba.proyecto_river_plate_junin.service;
+import java.util.List;
+
+import javax.management.loading.PrivateClassLoader;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ar.edu.unnoba.proyecto_river_plate_junin.model.Socio;
+import ar.edu.unnoba.proyecto_river_plate_junin.repository.SocioRepository;
 
 @Service
 public class SocioServiceImp implements SocioService{
+
+    @Autowired
+    private SocioRepository repository;
+    @Override
+    public List<Socio> getAllSocios() {
+        return repository.filterSocios();
+    
+    
+    }
 
 
     
