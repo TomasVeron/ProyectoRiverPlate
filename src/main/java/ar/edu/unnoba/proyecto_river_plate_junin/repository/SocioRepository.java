bd273aa82ce3c8f32520c7d14244b354ddd263be
facundojure <72893752+facundojure@users.noreply.github.com>
2021-12-08 19:59:11 -0300

    implementado listado de socio parte 2

--- src/main/java/ar/edu/unnoba/proyecto_river_plate_junin/repository/SocioRepository.java
+++ src/main/java/ar/edu/unnoba/proyecto_river_plate_junin/repository/SocioRepository.java
@@ -1,7 +1,11 @@
 package ar.edu.unnoba.proyecto_river_plate_junin.repository;
 
 import ar.edu.unnoba.proyecto_river_plate_junin.model.*;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
@@ -10,6 +14,11 @@ public interface SocioRepository extends JpaRepository<Socio, Long> {
 
 
     public Socio findByNombre(String nombre);
+    @Query(value = 
+        "SELECT * FROM socios s  order by s.estado, s.apellido, s.nombre", nativeQuery = true)
+    
+    
+    List<Socio> filterSocios();
 
 
 
--- src/main/java/ar/edu/unnoba/proyecto_river_plate_junin/service/SocioService.java
+++ src/main/java/ar/edu/unnoba/proyecto_river_plate_junin/service/SocioService.java
@@ -1,7 +1,14 @@
 package ar.edu.unnoba.proyecto_river_plate_junin.service;
 import org.springframework.stereotype.Service;
 
+import ar.edu.unnoba.proyecto_river_plate_junin.model.Socio;
+
+import java.util.List;
+
+
 @Service
 public interface SocioService {
+    public List<Socio> getAllSocios();
+    
 
 }
