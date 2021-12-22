39a8f49a4c1c98cccfba19fd787b62ffa52aa7aa
agust <agustin.ver0n.2018@gmail.com>
2021-12-22 20:52:22 -0300

    armado de categorias y  cuotas

---
+++ src/main/java/ar/edu/unnoba/proyecto_river_plate_junin/repository/CategoriaRepository.java
@@ -0,0 +1,5 @@
+package ar.edu.unnoba.proyecto_river_plate_junin.repository;
+
+public class CategoriaRepository {
+    
+}
---
+++ src/main/java/ar/edu/unnoba/proyecto_river_plate_junin/service/CategoriaService.java
@@ -0,0 +1,5 @@
+package ar.edu.unnoba.proyecto_river_plate_junin.service;
+
+public interface CategoriaService {
+    
+}
