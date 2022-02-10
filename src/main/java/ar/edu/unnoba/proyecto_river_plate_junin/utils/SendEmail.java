8073f85b41486a90d9509c570433f65e70d65e60
agust <agustin.ver0n.2018@gmail.com>
2022-02-09 21:15:05 -0300

    envio de mail con html y css

--- src/main/java/ar/edu/unnoba/proyecto_river_plate_junin/utils/SendEmail.java
+++ src/main/java/ar/edu/unnoba/proyecto_river_plate_junin/utils/SendEmail.java
@@ -1,12 +1,15 @@
 package ar.edu.unnoba.proyecto_river_plate_junin.utils;
 
-import java.io.File;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -33,27 +36,88 @@ public class SendEmail {
 
         // default = text/plain
         //helper.setText("Check attachment for image!");
-
-        // true = text/html
-        helper.setText("<h1>Nueva Cuota generada para el socio</h1>" + socio.getNombre() + " "+ socio.getApellido() +" con DNI " + socio.getDni()+"\n"+
-                        "<span><span>"
+        DateFormat formatter = new SimpleDateFormat("dd MMMM yyyy");
+        Calendar fechaEmision = Calendar.getInstance();
+        fechaEmision.setTime(cuota.getFechaCreacion());
+        Calendar fechaCaducidad = Calendar.getInstance();
+        fechaCaducidad.setTime(cuota.getFechaCaducidad());
         
-        
-        , true);
+        String fechaEmisionString = formatter.format(fechaEmision.getTime());
+        String fechaCaducidadString = formatter.format(fechaCaducidad.getTime());
+        int mesFechaEmision = fechaEmision.get(Calendar.MONTH) + 1;
 
-        File cuotaPdf = new File("cuotasPdf/cuota.pdf");
+        
+
+
+        String html = "<html xmlns:th='http://www.thymeleaf.org'>"+
+                        "<head>"+
+        
+                        "</head>"+
+                        "<body>"+
+                            "<div class='userDetails-container' style='display:flex;justify-content:space-between;padding:10px;'>"+
+                                "<h3 class='userDetails__detailName' style='display:block; padding: 10px;'>DNI</h3>"+
+                                "<span class='userDetails__detailInfo' style='display:block; padding: 10px;'>"+cuota.getSocio().getDni()+"</span>"+
+                            "</div>"+
+                            "<div class='userDetails-container' style='display:flex;justify-content:space-between;padding:10px;'>"+
+                                "<h3 class='userDetails__detailName' style='display:block; padding: 10px;'>Nombre</h3>"+
+                                "<span class='userDetails__detailInfo' style='display:block; padding: 10px;'>"+cuota.getSocio().getNombre()+"</span>"+
+                            "</div>"+
+                            "<div class='userDetails-container' style='display:flex;justify-content:space-between;padding:10px;'>"+
+                                "<h3 class='userDetails__detailName' style='display:block; padding: 10px;'>Apellido</h3>"+
+                                "<span class='userDetails__detailInfo' style='display:block; padding: 10px;'>"+cuota.getSocio().getApellido()+"</span>"+
+                            "</div>"+
+                            "<div class='userDetails-container' style='display:flex;justify-content:space-between;padding:10px;'>"+
+                                "<h3 class='userDetails__detailName' style='display:block; padding: 10px;'>Domicilio</h3>"+
+                                "<span class='userDetails__detailInfo' style='display:block; padding: 10px;'>"+cuota.getSocio().getDomicilio()+"</span>"+
+                            "</div>"+
+                            "<div class='userDetails-container' style='display:flex;justify-content:space-between;padding:10px;'>"+
+                                "<h3 class='userDetails__detailName' style='display:block; padding: 10px;'>Codigo</h3>"+
+                                "<span class='userDetails__detailInfo' style='display:block; padding: 10px;'>"+cuota.getSocio().getCodigo()+"</span>"+
+                            "</div>"+
+                            "<div class='userDetails-container' style='display:flex;justify-content:space-between;padding:10px;'>"+
+                                "<h3 class='userDetails__detailName' style='display:block; padding: 10px;'>Cuota Mes</h3>"+
+                                "<span class='userDetails__detailInfo' style='display:block; padding: 10px;'>"+mesFechaEmision+"</span>"+
+                            "</div>"+
+                            "<div class='userDetails-container' style='display:flex;justify-content:space-between;padding:10px;'>"+
+                                "<h3 class='userDetails__detailName' style='display:block; padding: 10px;'>Fecha Caducidad</h3>"+
+                                "<span class='userDetails__detailInfo' style='display:block; padding: 10px;'>"+fechaCaducidadString+"</span>"+
+                            "</div>"+
+                            "<div class='userDetails-container' style='display:flex;justify-content:space-between;padding:10px;'>"+
+                                "<h3 class='userDetails__detailName' style='display:block; padding: 10px;'>Fecha Emision</h3>"+
+                                "<span class='userDetails__detailInfo' style='display:block; padding: 10px;'>"+fechaEmisionString+"</span>"+
+                            "</div>"+
+                            "<div class='userDetails-container' style='display:flex;justify-content:space-between;padding:10px;'>"+
+                                "<h3 class='userDetails__detailName' style='display:block; padding: 10px;'>Importe</h3>"+
+                                "<span class='userDetails__detailInfo' style='display:block; padding: 10px;'>"+"$"+cuota.getImporte()+"</span>"+
+                            "</div>"+
+                            "<div class='userDetails-container' style='display:flex;justify-content:space-between;padding:10px;'>"+
+                                "<h3 class='userDetails__detailName' style='display:block; padding: 10px;'>Categoria</h3>"+
+                                "<span class='userDetails__detailInfo' style='display:block; padding: 10px;'>"+cuota.getCategoria().getNombre()+"</span>"+
+                            "</div>"+
+                        "</body>"+
+                    "</html>";
+
+        // File style = new File("src/main/resources/static/css/cuotaPdf.css");
 		// hard coded a file path
-        FileSystemResource file = new FileSystemResource(cuotaPdf);
+        // FileSystemResource file = new FileSystemResource(style);
+        // true = text/html
+        // helper.addAttachment("style.css", file);
+        helper.setText(html, true);
+
+
+        // File cuotaPdf = new File("cuotasPdf/cuota.pdf");
+		// // hard coded a file path
+        // FileSystemResource file = new FileSystemResource(cuotaPdf);
 
         
 
-        helper.addAttachment("cuota.pdf", file);
+        // helper.addAttachment("cuota.pdf", file);
         
         
 
         javaMailSender.send(msg);
         
-        cuotaPdf.delete();
+        // cuotaPdf.delete();
     }
     
 
